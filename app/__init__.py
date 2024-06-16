43b72fd64d18e530573dfa7dd1e60bcbdaa7f05a
pablo <p.balastegui@alumno.um.edu.ar>
2024-06-15 23:14:31 -0300

    cache funcionando con redis

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ import os
 from app.config import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_caching import Cache
 
 db = SQLAlchemy()
 ma = Marshmallow()
+cache = Cache()
 
 def create_app():
     app = Flask(__name__)
@@ -19,6 +21,10 @@ def create_app():
     f.init_app(app) #inicia la app con el modo seleccionado
     db.init_app(app)
     ma.init_app(app)
+
+    redis_host = os.getenv('REDIS_HOST')
+    redis_password = os.getenv('REDIS_PASSWORD')
+    cache.init_app(app, config={'CACHE_TYPE': 'RedisCache', 'CACHE_DEFAULT_TIMEOUT': 300, 'CACHE_REDIS_HOST': redis_host, 'CACHE_REDIS_PORT': '6379', 'CACHE_REDIS_DB': '0', 'CACHE_REDIS_PASSWORD': redis_password, 'CACHE_KEY_PREFIX': 'user_'})
 
     from app.resources import user
     app.register_blueprint(user, url_prefix='/api/v1/user')
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,7 @@
 from app.models import User
 from app.repositories import UserRepository
 from app.services.security_service import SecurityService
-
+from app import cache
 
 class UserService:
     """
@@ -14,23 +14,48 @@ class UserService:
     
     def create(self, entity: User) -> User:
         entity.password = SecurityService.generate_password(entity.password)
-        return self.__repo.create(entity)
+        user = self.__repo.create(entity)
+        cache.set(f'{user.id}', user, timeout=50)
+        return user
     
     def update(self, entity: User, id: int) -> User:
         entity.password = SecurityService.generate_password(entity.password)
-        return self.__repo.update(entity, id)
+        user = cache.get(f'{id}')
+        if user:
+            cache.update(f'{user.id}', entity, timeout=50)
+            user = self.__repo.update(entity, id)
+        else:
+            user = self.__repo.update(entity, id)
+            cache.set(f'{user.id}', user, timeout=50)
+        return user
     
     def delete(self, id: int) -> User:
+        user = cache.get(f'{id}')
+        if user:
+            cache.delete(f'{user.id}')
         return self.__repo.delete(id)
     
+    @cache.memoize(timeout=50)
     def find_all(self) -> list:
         return self.__repo.find_all()
     
     def find_by_id(self, id: int) -> User:
-        return self.__repo.find_by_id(id)
+        user = cache.get(f'{id}')
+        if user is None:
+            user = self.__repo.find_by_id(id)
+            if not user:
+                return None
+            cache.set(f'{user.id}', user, timeout=50)
+        return user
     
     def find_by_email(self, email: str) -> User:
-        return self.__repo.find_by_email(email)
+        user = cache.get(f'{email}')
+        if not user:
+            user = self.__repo.find_by_email(email)
+            if not user:
+                return None
+            cache.set(f'{user.email}', user, timeout=50)
+        return user
     
     def check_password(self, mail: str, password: str) -> bool:
         user = self.__repo.find_by_email(mail)
